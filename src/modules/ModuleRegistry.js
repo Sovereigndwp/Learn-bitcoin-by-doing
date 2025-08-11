e87608adfaf7f7c109f08f6ccb4fde35f666cc10
Sovereigndwp <sovereigndwp@proton.me>
2025-08-11 15:14:26 -0400

    Revert "Complete Fundamentals sequence with BitcoinBlueprintModule"
    This reverts commit ae100a073d38979146a6b3f99fa86d23377f4a7e.

--- src/modules/ModuleRegistry.js
+++ src/modules/ModuleRegistry.js
@@ -4,7 +4,6 @@ import WhyBitcoinMattersModule from './WhyBitcoinMattersModule';
 import MoneyJourneyModule from './fundamentals/MoneyJourneyModule';
 import GoodMoneyModule from './fundamentals/GoodMoneyModule';
 import MoneyGoesBadModule from './fundamentals/MoneyGoesBadModule';
-import BitcoinBlueprintModule from './fundamentals/BitcoinBlueprintModule';
 import NumbersModule from './NumbersModule';
 import HashingModule from './HashingModule';
 import MiningModule from './MiningModule';
@@ -60,7 +59,7 @@ export const moduleRegistry = {
     id: 'bitcoin-blueprint',
     title: 'Bitcoin: The Better Blueprint',
     description: 'Discover how Bitcoin addresses every money failure through superior design.',
-    component: BitcoinBlueprintModule,
+    component: CustodyModule,
     order: 5,
     group: 'fundamentals',
     prerequisites: ['money-goes-bad']
